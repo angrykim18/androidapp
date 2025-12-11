56088c95d1fa02f3e0601f8af6585c77f56d7fd8
angrykim18 <khbkhs05@gmail.com>
2025-12-11 12:35:34 +0900

    Delete src/main directory

--- src/main/java/com/example/newez/model/CategoryDto.kt
+++
@@ -1,8 +0,0 @@
-package com.example.newez.model
-
-// ✅ [추가] 영화 하위 카테고리 데이터를 담을 클래스
-data class CategoryDto(
-    val id: Long,
-    val name: String
-
-)
--- src/main/java/com/example/newez/model/IpUpdateRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.newez.model
-
-import com.google.gson.annotations.SerializedName
-
-data class IpUpdateRequest(
-    @SerializedName("ip")
-    val ip: String
-)
